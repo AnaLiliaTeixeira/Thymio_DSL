fe6ddeeb899d2edc5673620c2383e06a45c8ea76
DanielLopesME <dlopes@maiseficaz.pt>
2024-05-23 11:01:41 +0100

    boas

--- org.xtext.project.tdsl.ui/src/org/xtext/project/tdsl/ui/quickfix/TDslQuickfixProvider.java
+++ org.xtext.project.tdsl.ui/src/org/xtext/project/tdsl/ui/quickfix/TDslQuickfixProvider.java
@@ -3,8 +3,12 @@
  */
 package org.xtext.project.tdsl.ui.quickfix;
 
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider;
 import org.eclipse.xtext.ui.editor.quickfix.Fix;
+import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
+import org.eclipse.xtext.validation.Issue;
 import org.xtext.project.tdsl.validation.TDslValidator;
 
 /**
@@ -24,5 +28,55 @@ public class TDslQuickfixProvider extends DefaultQuickfixProvider {
 //			}
 //		});
 //	}
+	
+	@Fix(TDslValidator.DUPLICATE_ACTION_WARNING)
+	public void removeDuplicateAction(final Issue issue, IssueResolutionAcceptor acceptor) {
+	    acceptor.accept(issue, "Remove repeted action", "Removed repeted action", null, context -> {
+	    	removeRepeted(context.getXtextDocument(), issue, "");
+	    });
+	}
+	
+	@Fix(TDslValidator.DUPLICATE_SENSORIF_WARNING)
+	public void removeDuplicateSensor(final Issue issue, IssueResolutionAcceptor acceptor) {
+	    acceptor.accept(issue, "Remove repeted sensor", "Removed repeted sensor", null, context -> {
+	    	removeRepeted(context.getXtextDocument(), issue, "");
+	    });
+	}
+	
+	@Fix(TDslValidator.DUPLICATE_BUTTON_WARNING)
+	public void removeDuplicateButtonSensor(final Issue issue, IssueResolutionAcceptor acceptor) {
+	    acceptor.accept(issue, "Remove repeted button", "Removed repeted button", null, context -> {
+	    	removeRepeted(context.getXtextDocument(), issue, "");
+	    });
+	}
+	
+	@Fix(TDslValidator.SPEED_GT_500_WARNING)
+	public void setSpeedTo500(final Issue issue, IssueResolutionAcceptor acceptor) {
+	    acceptor.accept(issue, "Set speed to 500", "Set speed to 500", null, context -> {
+	    	removeRepeted(context.getXtextDocument(), issue, "500");
+	    });
+	}
+	
+
+	
+	
+	@Fix(TDslValidator.NEGATIVE_SPEED_WARNING)
+	public void updateNegativeValues(final Issue issue, IssueResolutionAcceptor acceptor) {
+	    acceptor.accept(issue, "Insert a positive value", "Inserte a positive value", null, context -> {
+	    	removeRepeted(context.getXtextDocument(), issue, removeMinus(context.getXtextDocument(),issue));
+	    });
+	}
+	
+
+	
+	
+	protected void removeRepeted(IXtextDocument iXtextDocument, Issue issue, String newSpecifier) throws BadLocationException {
+        iXtextDocument.replace(issue.getOffset(), issue.getLength(), newSpecifier);
+	}
+	
+	protected String removeMinus(IXtextDocument iXtextDocument, Issue issue) throws BadLocationException {
+	    return iXtextDocument.get(issue.getOffset(), issue.getLength()).substring(1, issue.getLength());
+	}
+	
 
 }
